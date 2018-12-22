7aba68beb8d54c379037da543cb0cc23ca74b1f5
M-Sin <michael.sinclair@ryerson.ca>
2018-12-22 16:01:58 -0500

    V 1.13
    Still working on multi-node parallel resistors

--- CircuitBuilder/src/circuit/CircuitAnalysis.java
+++ CircuitBuilder/src/circuit/CircuitAnalysis.java
@@ -40,18 +40,12 @@ public class CircuitAnalysis {
 	
 	/* automate circuit measurements */
 	protected void analyzeCircuit() {
-		/* test print - remove later */
-		for (Node node:nodeList) {
-			System.out.println(node.toStringAttachments());
-		}
 		/* find total voltage and count voltage sources */
 		this.analyzeVoltage();
 		/* find total resistance of the circuit */
 		this.analyzeResistance();
-		/* test print - remove later */
-		for (Node node:nodeList) {
-			System.out.println(node.toStringAttachments());
-		}
+		/* print out equivalent components */
+		System.out.println(components.toString());
 		/* print out calculated circuit characteristics */
 		this.printCharacteristics();
 	}
@@ -74,20 +68,12 @@ public class CircuitAnalysis {
 	protected void analyzeResistance() {
 		/* combine parallel resistors between the same two nodes into one equivalent resistor */
 		this.analyzeParallelSameNode();
-		/* test print */
-		System.out.println("Components size is " + components.size());
 		
-		/* while circuit has not been reduced to directly serial voltages and one mega-equivalent resistor */
-		// UNDER CONSTRUCTION
-		//while(components.size()>this.VoltageSources+1) {
-			//System.out.println("Components " + components.size());
-			/* combine any existing serial resistors */
-			//UNDER CONSTRUCTION
-			//this.analyzeSerialResistances();
-			/* while parallel resistors still exist, combine them */
-			// UNDER CONSTRUCTION
-			//this.analyzeParallelMultiNode(nodeList.get(0));
-		//}
+		/* test print */
+		System.out.println("Circuit contains "+components.size());
+		
+		/* combine serial and multi-node parallel resistors until only 1 mega-equivalent resistor remains */
+		//to do
 		
 		/* now that all resistors are serial resistors, for each component */
 		for (int i = 0; i<components.size();i++) {
@@ -103,12 +89,13 @@ public class CircuitAnalysis {
 	protected void analyzeParallelSameNode() {
 		/* starting with all resistors connected between the same two nodes */
 		ArrayList<Component> temp = new ArrayList<>();
-		ArrayList<Component> removal = new ArrayList<>();
+		ArrayList<Component> toRemove = new ArrayList<>();
+		ArrayList<Component> toConnect = new ArrayList<>();
 		/* for each node */
 		for (int n = 0; n<nodeList.size();n++) {
 			/* find components connected to each other node */
 			for (int m = 0; m<nodeList.size();m++) {
-				/* components cannot have the same node on both sides */
+				/* components cannot have the same node on both sides & don't want to do the same two nodes twice */
 					if (n!=m && n<m) {
 						/* for each component */
 						for (int k = 0;k<components.size();k++) {
@@ -117,142 +104,72 @@ public class CircuitAnalysis {
 								if (components.get(k).getClass() == Resistor.class) {
 									/* if it is between the two nodes */
 									if(components.get(k).getNode1().getId() == n && components.get(k).getNode2().getId() == m) {
-										/* test print */
-										System.out.println("I am adding here");
 										/* add it to temporary list */
 										temp.add(components.get(k));
-										/* remove it from the nodes and from the component list */
-										components.get(k).getNode1().connect(components.get(k));
-										components.get(k).getNode2().connect(components.get(k));
-										/* track which components needs to be removed in place of equivalent single resistor */
-										removal.add(components.get(k));
-										components.remove(components.get(k));
-									}
-									/* if a parallel connection was found */
-									if(temp.size()>1) {
-										Resistor temporary = new Resistor(this.parallelResistors(temp),nodeList.get(n),nodeList.get(m));
-										components.add(temporary);
-										temporary.getNode1().connect(temporary);
-										temporary.getNode2().connect(temporary);
+										System.out.println("Temp stored: "+components.get(k).toString());
 									}
 								}
 							}
 						}
+						/* if a parallel connection was found between node n and m*/
+						if(temp.size()>1) {
+							/* test print */
+							System.out.println("Node1 "+n+" Node2 "+m);
+							/* create equivalent parallel resistor */
+							Resistor equivalent = new Resistor(this.parallelResistors(temp),this.findNode(n),this.findNode(m));
+							/* queue it for connection */
+							toConnect.add(equivalent);
+							/* queue resistors that need to be removed */
+							for(Component remove:temp) {
+								toRemove.add(remove);
+							}
+							temp.clear();
+							/* test print */
+							System.out.println("Temp cleared.");
+						}
+						temp.clear();
+						/* test print */
+						System.out.println("Temp cleared.");
 					}
 				}
 			}
 		/* remove resistors to be replaced by single equivalent resistor */
-		for (int i = 0; i<removal.size();i++) {
-			if(components.contains(removal.get(i))){
-				removal.get(i).getNode1().disconnect(removal.get(i));
-				/* test print */
-				System.out.println("Removed "+removal.get(i));
-				removal.get(i).getNode2().disconnect(removal.get(i));
-				/* test print */
-				System.out.println("Removed "+removal.get(i));
-				components.remove(this.findIndex(components, removal.get(i)));
+		/* if there are items to be removed */
+		if(toRemove.size()>0) {
+			/* for each component to be removed */
+			for (Component remove:toRemove) {
+				/* for each component */
+				for(int i = 0; i <components.size();i++) {
+					/* if the component is a resistor and it is in the list of resistors to be removed */
+					if(components.get(i).getId()==remove.getId()&&components.get(i) instanceof Resistor) {
+						/* remove it from components */
+						remove.getNode1().disconnect(remove);
+						remove.getNode2().disconnect(remove);
+						/* test print */
+						System.out.println("Removed "+components.get(i).toString());
+						components.remove(i);
+						/* need to consider that components has shrunk by 1 */
+						i--;
+					}
+				}
 			}
+		}
+		/* attach equivalent resistors */
+		for(Component comp:toConnect) {
+			components.add(comp);
+			comp.getNode1().connect(comp);
+			comp.getNode2().connect(comp);
 		}
 	}
 	
 	/* UNDER CONSTRUCTION */
 	protected void analyzeParallelMultiNode(Node start) {
-		/* for each component */
-		for (Component comp:this.components) {
-			/* looking forwards */
-			if(comp.getNode1().getId()>start.getId())
-				/* if it is a resistor */
-				if(comp.getClass()==Resistor.class) {
-					/* if its first node has more than 1 connection and so is in parallel */
-					if (comp.getNode1().getAttachments().size()>2) {
-						/* for each attachment */
-						for (int i = 0; i<comp.getNode1().getAttachments().size();i++) {
-							/* looking forwards */
-							if (comp.getNode1().getAttachments().get(i).getNode2().getId()>comp.getNode1().getId()) {
-								/* find the node to which they connect together again */
-								ArrayList<ArrayList<Node>> Temp = new ArrayList<>();
-								/* for each component attached in parallel to this node */
-								for(Component comps:comp.getNode1().getAttachments()) {
-									/* if it is a resistor - given that this program currently does not support parallel voltages */
-									if (comps.getClass()==Resistor.class){
-										/* if not the 'most parallel' meaning stretching across the farthest nodes */
-										
-									}
-								}
-							}
-						}
-					}	
-				}
-			}
+
 		}
 	
 	/* UNDER CONSTRUCTION */
 	protected void analyzeSerialResistances() {
-		/* temporary ArrayList to store components to be removed */
-		ArrayList<Component> temp = new ArrayList<>();
-		/* for each component */
-		for(int j = 0;j<components.size();j++) {
-			/* for each other component */
-			for(int i = 0; i<components.size();i++) {
-				/* node 0 requires a special case - would consider also eliminating the 0th node as an ID */
-				if(components.get(j).getNode1().getId()!=0) {
-					/* obviously do not consider the same component */
-					if(i!=j && i<j) {
-						System.out.println("1");
-						/* for two resistors */
-						if(components.get(i).getClass() == Resistor.class && components.get(j).getClass()==Resistor.class) {
-							/* if not already reduced (this method will need to be run repeatedly if multiple resistors are in series */
-							if(!temp.contains(components.get(i))&&!temp.contains(components.get(j))) {
-								System.out.println("2");
-								System.out.println("comp1 node2ID "+ components.get(i).getNode2().getId());
-								System.out.println("comp2 node1ID "+ components.get(j).getNode1().getId());
-								System.out.println("attachments comp1 "+ components.get(i).getNode2().getAttachments().size());
-								System.out.println("attachments comp2 "+ components.get(j).getNode1().getAttachments().size());
-								/* condition for two serial resistors */
-								if(components.get(i).getNode2().getId() == components.get(j).getNode1().getId() && components.get(i).getNode2().getAttachments().size()==2 && components.get(j).getNode1().getAttachments().size()==2) {
-									temp.add(components.get(i));
-									temp.add(components.get(j));
-									Resistor equivalent = new Resistor(((Resistor)components.get(i)).getR(),((Resistor)components.get(i)).getNode1(),((Resistor)components.get(j)).getNode2());
-									components.add(equivalent);
-									System.out.println("Added: "+equivalent.toString());
-								}
-							}
-						}
-					}
-				}
-				else {
-					/* obviously do not consider the same component */
-					if(i!=j && i<j) {
-						System.out.println("1");
-						/* for two resistors */
-						if(components.get(i).getClass() == Resistor.class && components.get(j).getClass()==Resistor.class) {
-							/* if not already reduced during THIS iteration of this method (this method will need to be run repeatedly if multiple resistors are in series */
-							if(!temp.contains(components.get(i))&&!temp.contains(components.get(j))) {
-								System.out.println("2");
-								System.out.println("comp1 node2ID "+ components.get(i).getNode2().getId());
-								System.out.println("comp2 node2ID "+ components.get(j).getNode2().getId());
-								System.out.println("attachments comp1 "+ components.get(i).getNode2().getAttachments().size());
-								System.out.println("attachments comp2 "+ components.get(j).getNode2().getAttachments().size());
-								/* condition for two serial resistors under special case where second resistor node 1 is 0 and node 2 is node N*/
-								if(components.get(i).getNode2().getId() == components.get(j).getNode2().getId() && components.get(i).getNode2().getAttachments().size()==2 && components.get(j).getNode2().getAttachments().size()==2) {
-									temp.add(components.get(i));
-									temp.add(components.get(j));
-									Resistor equivalent = new Resistor(((Resistor)components.get(i)).getR(),((Resistor)components.get(i)).getNode1(),((Resistor)components.get(j)).getNode2());
-									components.add(equivalent);
-									System.out.println("Added: "+equivalent.toString());
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		/* remove resistors replaced by single equivalent resistors */
-		for(Component comp:temp) {
-			components.remove(comp);
-			/* test print */
-			System.out.println("Removed "+comp.toString());
-		}
+
 	}
 	
 	/* Find ArrayList index */
@@ -265,6 +182,26 @@ public class CircuitAnalysis {
 			}
 		}
 		return i;
+	}
+	
+	/* find node based on id */
+	protected Node findNode(int id) {
+		/* value to store index */
+		int i  = 0;
+		/* for each node */
+		for(Node node:nodeList) {
+			/* if it does not equal the desired node */
+			if(node.getId()!=id) {
+				/* increase the index */
+				i++;
+			}
+			/* if it does */
+			else {
+				/* stop searching */
+				break;
+			}
+		}
+		return nodeList.get(i);
 	}
 	
 	/* Calculate parallel resistance */
